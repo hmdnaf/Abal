66528e5f41bb09def4e222e167c7b34e56fc3adc
Hamdan AF <110399823+hmdnaf@users.noreply.github.com>
2024-05-11 13:32:56 +0700

    Add files via upload

---
+++ html-css-javascript/data.js
@@ -0,0 +1,57 @@
+// const nama = "Hamdan Al Fattah";
+// let usia = 19;
+
+// let biodata = document.getElementById('biodata');
+
+// function generateBiodata() {
+//     let generasi;
+
+//     if(usia > 10 && usia < 17) { 
+//         generasi = "generasi remaja";
+//     } 
+//     else if (usia > 17 && usia < 30) {
+//         generasi = "generasi dewasa";
+//     } 
+//     else if (usia > 30) {
+//         generasi = "generasi tua";
+//     }
+//     else if (usia < 5) {
+//         generasi = "generasi blita";
+//     }
+//     else { 
+//         generasi = "generasi anak anak";
+//     }
+//     return biodata.innerHTML = generasi;
+// }
+
+// console.log(nama);
+// console.log(usia);
+
+// generateBiodata();
+
+// const nama = "Hamdan Al Fattah";
+// let tahunLahir = 2011;
+
+// function generateBiodata() {
+
+//     if(tahunLahir > 1946 && tahunLahir < 1960) {
+//         console.log("generasi anda adalah Baby Boomers");
+//     }
+//     else if(tahunLahir > 1961 && tahunLahir < 1980) {
+//         console.log("generasi anda adalah X");
+//     }
+//     else if(tahunLahir > 1981 && tahunLahir < 1990) {
+//         console.log("generasi anda adalah Milenial");
+//     }
+//     else if(tahunLahir > 1991 && tahunLahir < 2010) {
+//         console.log("generasi anda adalah Z");
+//     }
+//     else {
+//         console.log("generasi anda adalah alpha");
+//     }
+// }
+// console.log(nama);
+// console.log(tahunLahir);
+   
+// generateBiodata();
+
